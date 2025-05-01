f47187420c9cc0b8f35eee819435a5e94faf4f96
squishedfox <squishedfox@protonmail.com>
2025-05-01 08:03:42 -0500

    update query to pull back additional data

--- src/features/carriers/table.tsx
+++ src/features/carriers/table.tsx
@@ -3,13 +3,15 @@ import { gql, NetworkStatus, useQuery } from "@apollo/client";
 const GET_CARRIERS = gql`
   query {
     carriers {
-      id
+      id,
+      db,
+      name
     }
   }
 `;
 
 const Table = () => {
-  const {data, loading, networkStatus} = useQuery(GET_CARRIERS, {})
+  const {data, loading} = useQuery(GET_CARRIERS, {})
 
   if (loading) {
     return (<div><p>Loading...</p></div>);
